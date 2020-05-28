9207f5988116642db79c58fc712b22023110d02f
AnSm <79778735356@ya.ru>
2020-05-28 07:23:40 +0300

    1com

---
+++ 1.bat
@@ -0,0 +1,25 @@
+echo on
+md Smirnova
+cd Smirnova
+md Nastya
+cd Nastya
+md Ruslanovna
+pause
+cd Ruslanovna
+echo> 0401200.txt
+cd ..
+cd ..
+cd ..
+pause
+del Smirnova /S /Q /F
+pause
+cd Smirnova
+cd Nastya
+rd Ruslanovna
+cd ..
+rd Nastya
+cd ..
+rd Smirnova
+pause
+
+
---
+++ 2.bat
@@ -0,0 +1,9 @@
+echo on
+cd Smirnova
+cd Nastya
+rd Ruslanovna
+cd ..
+rd Nastya
+cd ..
+rd Smirnova
+pause
